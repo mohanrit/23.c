ec03c7e59aaac1ce3bc88145c47b01bb85a92921
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-01-24 16:39:23 +0530

    Create 271.c

---
+++ 271.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n=2,i;
+    for(i=1;i<=10;i++)
+    {
+        if((n%i==0) ||(n%i!=0))
+        {
+            printf("it is number");
+        }
+    }
+}
