e7770b2842648526f167b694ef7a837376b006c9
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-01-24 16:14:39 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+
+int array[];
+printf("enter  the value of num\n");
+scanf("%d",&num);
+
+printf("%d",&num);
+printf("enter the elements one by one \n");
+for(i=0;i<num;i++)
+
+{
+scanf("%d",&array is \n");
+}
+printf("input array is \n");
+for(i=o;i<num;i++)
+
+[
+for(b=0;b<(num a-1);b++)
+{
+for(b=0;b<num-a-1);b++)
+}
+}
